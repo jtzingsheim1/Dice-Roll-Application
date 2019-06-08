370781d5f87b13b1e855ff5ab775ea1542dc0178
jtzingsheim1 <43869157+jtzingsheim1@users.noreply.github.com>
2019-06-07 21:40:56 -0500

    Got the basics of the RollButton function working

--- app.R
+++ app.R
@@ -13,6 +13,7 @@
 
 
 library(shiny)
+library(tidyverse)
 #library(DT)
 
 
@@ -32,17 +33,17 @@ ui <- fluidPage(
             # Sliders for wagers
             sliderInput("wager2",
                         "Wager on 2:",
-                        value = 0,
+                        value = 10,
                         min = 0,
                         max = 100),
             sliderInput("wager3",
                          "Wager on 3:",
-                         value = 0,
+                         value = 10,
                          min = 0,
                          max = 100),
             sliderInput("wager4",
                          "Wager on 4:",
-                         value = 0,
+                         value = 10,
                          min = 0,
                          max = 100),
             
@@ -65,10 +66,11 @@ ui <- fluidPage(
 )
 
 
-dice.values <- c(0, 0)
+# dice.values <- c(0, 0)
 
-# RollDice <- function() {
-#     sample(1:6, 2, replace = TRUE)
+
+# RollButton <- function() {
+#     dice.values2 <- sample(1:6, 2, replace = TRUE)
 # }
 
 # Server Logic------------------------------------------------------------------
@@ -76,7 +78,7 @@ dice.values <- c(0, 0)
 server <- function(input, output) {
     
     # Reactive expression to create data frame summary
-    wager.values <- reactive({
+    WagerValues <- reactive({
         data.frame(
             Name = c("Total Amount Wagered"),
             Value = as.character(c(
@@ -86,17 +88,36 @@ server <- function(input, output) {
     
     # Show the wager details in an HTML table
     output$total.wager <- renderTable({
-        wager.values()
+        WagerValues()
     })
     
+    # # Get dice outcomes when Roll button is clicked
+    # observeEvent(input$roll, {
+    #     dice.values2 <- sample(1:6, 2, replace = TRUE)
+    #     return(dice.values)
+    # }, ignoreNULL = FALSE)
+    
     # Get dice outcomes when Roll button is clicked
-    RollDice <- eventReactive(input$roll, {
-        dice.values2 <- sample(1:6, 2, replace = TRUE)
+    RollButton <- eventReactive(input$roll, {
+        
+        dice.values <- sample(1:6, 2, replace = TRUE)
+        dice.sum <- sum(dice.values)
+        
+        outcome <- c(2:12)
+        wagers <- c(rep(0, 4), input$wager2, input$wager3, input$wager4, rep(0, 4))
+        wager.table <- data.frame(outcome, wagers)
+        
+        win.wager <- filter(wager.table, outcome == dice.sum)[[2]]
+        payout <- 5 * win.wager
+        losses <- sum(select(wager.table, wagers)) - win.wager
+        net.gain <- payout - losses
+        c(dice.values, dice.sum, net.gain)
+    
     }, ignoreNULL = FALSE)
     
     # Show the dice values
     output$dice.outcome <- renderText({
-        c(dice.values, RollDice(), dice.values2)
+        RollButton()
     })
     
 
