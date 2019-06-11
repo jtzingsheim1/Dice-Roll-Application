a7f89353095f3a39391025b511259c10cdee20ff
jtzingsheim1 <43869157+jtzingsheim1@users.noreply.github.com>
2019-06-10 21:25:29 -0500

    Finished cleaning up formatting

--- app.R
+++ app.R
@@ -13,180 +13,114 @@
 
 
 library(shiny)
-library(tidyverse)
+
+
+# Set configuration variables
+slider.def <- 0  # Slider default value
+slider.min <- 0  # Slider minimum
+slider.max <- 100  # Slider maximum
+img.sz <- "100px"  # Image sizes
 
 
 # User Interface----------------------------------------------------------------
-
-# Set configuration variables
-slider.def <- 0
-slider.min <- 0
-slider.max <- 100
-img.sz <- "100px"
 
 ui <- fluidPage(
 
     # Application title
     titlePanel("Dice Roll!"),
+    
+    # Set layout style to sidebar
+    sidebarLayout(
         
-        fluidRow(column(2,
-                        
-                        # Display instructions
-                        textOutput("instructions"),
-                        
-                        br(),
-                        
-                        # Display the payout table
-                        tableOutput("payout.table"),
-                        
-                        # Display token and wager details
-                        tableOutput("tokens.wagers"),
-                        
-                        # Button to roll the dice
-                        actionButton("roll", "Roll Dice!")),
-                        
-                 column(2,
-                       
-                       
-                        # Sliders for wagers
-                        sliderInput("wager2", "Wager on 2:",
-                                    value = slider.def,
-                                    min = slider.min,
-                                    max = slider.max),
-                        sliderInput("wager3", "Wager on 3:",
-                                    value = slider.def,
-                                    min = slider.min,
-                                    max = slider.max),
-                        sliderInput("wager4", "Wager on 4:",
-                                    value = slider.def,
-                                    min = slider.min,
-                                    max = slider.max),
-                        sliderInput("wager5", "Wager on 5:",
-                                    value = slider.def,
-                                    min = slider.min,
-                                    max = slider.max),
-                        sliderInput("wager6", "Wager on 6:",
-                                    value = slider.def,
-                                    min = slider.min,
-                                    max = slider.max),
-                        sliderInput("wager7", "Wager on 7:",
-                                    value = slider.def,
-                                    min = slider.min,
-                                    max = slider.max),
-                        sliderInput("wager8", "Wager on 8:",
-                                    value = slider.def,
-                                    min = slider.min,
-                                    max = slider.max),
-                        sliderInput("wager9", "Wager on 9:",
-                                    value = slider.def,
-                                    min = slider.min,
-                                    max = slider.max),
-                        sliderInput("wager10", "Wager on 10:",
-                                    value = slider.def,
-                                    min = slider.min,
-                                    max = slider.max),
-                        sliderInput("wager11", "Wager on 11:",
-                                    value = slider.def,
-                                    min = slider.min,
-                                    max = slider.max),
-                        sliderInput("wager12", "Wager on 12:",
-                                    value = slider.def,
-                                    min = slider.min,
-                                    max = slider.max),
-                        br(), br(), br()),
-        
-                 column(3,
+        sidebarPanel(
+            
+            fluidRow(
                 
-                        # Display stop message or dice results as applicable
-                        uiOutput("roll.output"))
+                # Left side of sidebar
+                column(6,
+
+                    # Display instructions
+                    textOutput("instructions"),
+                    
+                    br(),
+                    
+                    # Display the payout table
+                    tableOutput("payout.table"),
+                    
+                    # Display token and wager details
+                    tableOutput("tokens.wagers"),
+                    
+                    # Button to roll the dice
+                    actionButton("roll", "Roll Dice!")),
+                
+                # Right side of sidebar
+                column(6,
+                    
+                    # Sliders to place wagers
+                    sliderInput("wager2", "Wager on 2:",
+                                value = slider.def,
+                                min = slider.min,
+                                max = slider.max),
+                    sliderInput("wager3", "Wager on 3:",
+                                value = slider.def,
+                                min = slider.min,
+                                max = slider.max),
+                    sliderInput("wager4", "Wager on 4:",
+                                value = slider.def,
+                                min = slider.min,
+                                max = slider.max),
+                    sliderInput("wager5", "Wager on 5:",
+                                value = slider.def,
+                                min = slider.min,
+                                max = slider.max),
+                    sliderInput("wager6", "Wager on 6:",
+                                value = slider.def,
+                                min = slider.min,
+                                max = slider.max),
+                    sliderInput("wager7", "Wager on 7:",
+                                value = slider.def,
+                                min = slider.min,
+                                max = slider.max),
+                    sliderInput("wager8", "Wager on 8:",
+                                value = slider.def,
+                                min = slider.min,
+                                max = slider.max),
+                    sliderInput("wager9", "Wager on 9:",
+                                value = slider.def,
+                                min = slider.min,
+                                max = slider.max),
+                    sliderInput("wager10", "Wager on 10:",
+                                value = slider.def,
+                                min = slider.min,
+                                max = slider.max),
+                    sliderInput("wager11", "Wager on 11:",
+                                value = slider.def,
+                                min = slider.min,
+                                max = slider.max),
+                    sliderInput("wager12", "Wager on 12:",
+                                value = slider.def,
+                                min = slider.min,
+                                max = slider.max)
+                    )
+                )
+            ),
+        
+        # Main panel where output will be displayed
+        mainPanel(uiOutput("roll.output"))
 
         )
-
-)
+    )
 
 
 # Server Logic------------------------------------------------------------------
 
 server <- function(input, output) {
     
-    # Setup shared variables
-    vars <- reactiveValues()
-        vars$tokens <- 1000  # Initial number of tokens
-
-    # Assemble values and table of the payouts offered for each outcome
-    Payouts <- reactive({
-        print("Assembling payout values and table")
-        # Set the payout values
-        values <- as.integer(c(45, 20, 13, 9, 6, 4, 6, 9, 13, 20, 45))
-        # Assemble into table
-        table <- data.frame(
-            "Outcome" = 2:12,
-            "Payout Multiplier" = values)
-        list(values = values, table = table)
-    })
-    
-    # Monitor the total wager amount
-    Wagers <- reactive({
-        print("Updating amount wagered")
-        values <- c(input$wager2, input$wager3, input$wager4, input$wager5,
-                    input$wager6, input$wager7, input$wager8, input$wager9,
-                    input$wager10, input$wager11, input$wager12)
-        total <- sum(values)
-        print(paste("Current total wager:", total))
-        list(values = values, total = total)
-    })
-    
-    # Function to run when roll button is clicked
-    RollButton <- eventReactive(input$roll, {
+    # Setup tokens as a shared variable
+    variables <- reactiveValues()
+        variables$tokens <- 1000  # Initial number of tokens
+        variables$round <- 0  # Counter for number of rounds
         
-        print("Begin RollButton routine")
-        
-        # Check for sufficient tokens before rolling dice
-        if (Wagers()$total > vars$tokens) {
-            
-            print("Total wager exceeds total tokens, raise flag and stop")
-            warn.flag <- TRUE  # Set flag for output style
-            list(warn.flag = warn.flag)
-            
-        } else {
-            
-            print("Sufficient tokens to place wager, roll dice")
-            warn.flag <- FALSE  # Reset flag for output style
-            
-            # Roll the dice
-            die1 <- sample(1:6, 1)
-            #print(paste("First die:", die1))
-            die2 <- sample(1:6, 1)
-            #print(paste("Second die:", die2))
-            dice.sum <- sum(die1, die2)        
-            print(paste("Dice sum:", dice.sum))
-            
-            # Lookup winning wager and payout value
-            win.wager <- Wagers()$values[dice.sum - 1]
-            #print(paste("Winning wager:", win.wager))
-            payout <- Payouts()$values[dice.sum - 1]
-            #print(paste("Payout:", payout))
-            
-            # Calculate winnings, losses, net gain, and update token quantity
-            winnings <- payout * win.wager
-            #print(paste("Winnings:", winnings))
-            losses <- Wagers()$total - win.wager
-            #print(paste("Losses:", losses))
-            net.gain <- winnings - losses
-            print(paste("Net gain:", net.gain))
-            vars$tokens <- vars$tokens + net.gain
-            print(paste("Tokens:", vars$tokens))
-            
-            # Return all objects as a list
-            #print("Returning list of RollButton variables, end of Roll Button")
-            list(die1 = die1, die2 = die2, dice.sum = dice.sum,
-                 win.wager = win.wager, payout = payout, winnings = winnings,
-                 losses = losses, net.gain = net.gain, warn.flag = warn.flag)
-            
-        }
-        
-    })
-    
     # Render the instructions message
     output$instructions <- renderText({
         print("Rendering instuctions")
@@ -199,22 +133,96 @@ server <- function(input, output) {
         luck!"
     })
     
-    # Render a table of the current tokens and total wager
-    output$tokens.wagers <- renderTable({
-        print("Rendering tokens and wagers table")
-        var.names <- c("Total Tokens", "Total Wagered", "Tokens Available")
-        tokens.available <- vars$tokens - Wagers()$total
-        variables <- as.integer(c(vars$tokens, Wagers()$total,
-                                  tokens.available))
-        data.frame(var.names, variables)
-    }, colnames = FALSE)
+    # Assemble values and table of the payouts offered for each outcome
+    Payouts <- reactive({
+        print("Assembling payout values and table")
+        # Set the payout values
+        values <- as.integer(c(45, 20, 13, 9, 6, 4, 6, 9, 13, 20, 45))
+        # Assemble into table
+        table <- data.frame(
+            "Outcome" = 2:12,
+            "Payout Multiplier" = values)
+        list(values = values, table = table)  # Returns as list
+    })
     
     # Render a table of the payouts offered for each outcome
     output$payout.table <- renderTable({
         print("Rendering payout table")
         Payouts()$table
     })
+    
+    # Monitor the total wager amount
+    Wagers <- reactive({
+        print("Updating amount wagered")
+        values <- c(input$wager2, input$wager3, input$wager4, input$wager5,
+                    input$wager6, input$wager7, input$wager8, input$wager9,
+                    input$wager10, input$wager11, input$wager12)
+        total <- sum(values)
+        print(paste("Current total wager:", total))
+        list(values = values, total = total)  # Returns as list
+    })
+    
+    # Render a table of the current tokens and total wager
+    output$tokens.wagers <- renderTable({
+        print("Rendering tokens and wagers table")
+        var.names <- c("Total Tokens", "Total Wagered", "Tokens Available")
+        tokens.available <- variables$tokens - Wagers()$total
+        variables <- as.integer(c(variables$tokens, Wagers()$total,
+                                  tokens.available))
+        data.frame(var.names, variables)
+    }, colnames = FALSE)
 
+    # Function to run when roll button is clicked
+    RollButton <- eventReactive(input$roll, {
+        
+        print("Begin RollButton routine")
+        
+        # Check for sufficient tokens before rolling dice
+        if (Wagers()$total > variables$tokens) {
+            
+            print("Total wager exceeds total tokens, raise flag and stop")
+            warn.flag <- TRUE  # Set flag for output style
+            list(warn.flag = warn.flag)  # Returns as list
+            
+        } else {
+            
+            print("Sufficient tokens to place wager, proceed")
+            warn.flag <- FALSE  # Reset flag for output style
+            variables$round <- variables$round + 1  # Increment to trigger roll
+            list(warn.flag = warn.flag)  # Returns as list
+            
+        }
+        
+    })
+    
+    RollTheDice <- eventReactive(variables$round, {
+
+        print("Rolling the dice")
+        
+        # Roll the dice
+        die1 <- sample(1:6, 1)
+        die2 <- sample(1:6, 1)
+        dice.sum <- sum(die1, die2)        
+        print(paste("Dice sum:", dice.sum))
+        
+        # Lookup winning wager and payout value
+        win.wager <- Wagers()$values[dice.sum - 1]
+        payout <- Payouts()$values[dice.sum - 1]
+
+        # Calculate winnings, losses, net gain, and update token quantity
+        winnings <- payout * win.wager
+        losses <- Wagers()$total - win.wager
+        net.gain <- winnings - losses
+        print(paste("Net gain:", net.gain))
+        variables$tokens <- variables$tokens + net.gain
+        print(paste("Tokens:", variables$tokens))
+        
+        # Return all objects as a list
+        list(die1 = die1, die2 = die2, dice.sum = dice.sum,
+             win.wager = win.wager, payout = payout, winnings = winnings,
+             losses = losses, net.gain = net.gain)
+    })
+    
     # Determine the output type based on wager validity
     output$roll.output <- renderUI({
         print("Attempting to prepare roll output")
@@ -223,13 +231,13 @@ server <- function(input, output) {
             textOutput("wager.message")
         } else {
             print("Setting output style to dice results")
-            
-            fluidRow(
-                column(3,
+            list(fluidRow(
+                column(1,
                        imageOutput("die1", width = img.sz, height = img.sz)),
-                column(3,
-                       imageOutput("die2", width = img.sz, height = img.sz)))
-            #fluidRow(br(), tableOutput("roll.results"))
+                column(1,
+                       imageOutput("die2", width = img.sz, height = img.sz))),
+                fluidRow(br(), br(), br(),
+                         tableOutput("roll.results")))
         }
     })
     
@@ -241,14 +249,14 @@ server <- function(input, output) {
      
     # Render the image for the first die
     output$die1 <- renderImage({
-        filename <- paste0("die", RollButton()$die1, ".png")
+        filename <- paste0("die", RollTheDice()$die1, ".png")
         filepath <- normalizePath(file.path('./die_images', filename))
         list(src = filepath, width = img.sz, height = img.sz)
     }, deleteFile = FALSE)
     
     # Render the image for the second die
     output$die2 <- renderImage({
-        filename <- paste0("die", RollButton()$die2, ".png")
+        filename <- paste0("die", RollTheDice()$die2, ".png")
         filepath <- normalizePath(file.path('./die_images', filename))
         list(src = filepath, width = img.sz, height = img.sz)
     }, deleteFile = FALSE)
@@ -259,9 +267,13 @@ server <- function(input, output) {
         var.names <- c("Dice Sum", "Wager on Outcome", "Outcome Payout",
                        "Amount Won", "Amount Lost", "Net Gain",
                        "Remaining Tokens")
-        variables <- as.integer(c(RollButton()$dice.sum, RollButton()$win.wager,
-                       RollButton()$payout, RollButton()$winnings,
-                       RollButton()$losses, RollButton()$net.gain, vars$tokens))
+        variables <- as.integer(c(RollTheDice()$dice.sum,
+                                  RollTheDice()$win.wager,
+                                  RollTheDice()$payout,
+                                  RollTheDice()$winnings,
+                                  RollTheDice()$losses,
+                                  RollTheDice()$net.gain,
+                                  variables$tokens))
         data.frame(var.names, variables)
     }, colnames = FALSE)
 
